6214e6f8f9c8b6a557bc4eab094f939ec2f68196
Skjalgn <skjalgn@stud.ntnu.no>
2024-02-28 16:37:01 +1000

    christian fixed images

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,6 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 HANDLER404 = 'projects.views.custom_404'
 HANDLER500 = 'projects.views.custom_500'
